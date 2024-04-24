75799da1512c93be48428c5abadc11991cff37ee
Kay Dent <Kay.Dent@softwire.com>
2024-04-24 17:13:08 +0100

    Changed answer to be an Array type which is joined at the end

--- index.ts
+++ index.ts
@@ -3,14 +3,23 @@ function divisibleBy(num: number, divisor: number): boolean { return num % divis
 const possibleFactors = [3, 5, 7, 11];
 
 const key: {[index: number]: Function } = {
-    3: (answer: string) => answer += 'Fizz',
-    5: (answer: string) => answer += 'Buzz',
-    7: (answer: string) => {
-        if (answer.length < 5) {
-           return answer += 'Bang'
-        }
+    3: (answer: string[]) => {
+        answer.push('Fizz');
+        return answer;
     },
-    11: (answer: string) => answer = 'Bong'
+    5: (answer: string[]) => {
+        answer.push('Buzz');
+        return answer;
+    },
+    7: (answer: string[]) => {
+        if (answer.length < 2) {
+          answer.push('Bang');
+          return answer;
+        } else {
+          return ['Bang'];
+        } 
+    },
+    11: (answer: string[]) => answer = ['Bong'],
 }
 
 const fizzBuzz = () => {  
@@ -23,17 +32,17 @@ const fizzBuzz = () => {
         }
     })
 
-    let answer: string = '';
+    let answer: string[] = [];
 
     if (factors.length > 0) {
       for (let i = 0; i < factors.length; i++) {
         answer = key[factors[i]](answer)
       }
     } else {
-      answer = String(n)
+      answer.push(String(n))
     }
 
-    console.log(answer);
+    answer.length > 1 ? console.log(answer.join('')) : console.log(answer[0]);
   }
 }
 

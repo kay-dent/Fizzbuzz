2fa97af9cb1ab893d481963ced3eb894d120c5ce
Kay Dent <Kay.Dent@softwire.com>
2024-04-24 15:04:57 +0100

    If divisible by 5, it prints Buzz

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ for (let n = 0; n < 101; n++) {
     console.log('FizzBuzz')
   } else if (n % 3 === 0) {
     console.log('Fizz')
+  } else if (n % 5 == 0) {
+    console.log('Buzz')
   } else {
     console.log(n);
   }

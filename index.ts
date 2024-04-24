c4ddfa0f37baacc6f73daedb596a0aead645d182
Kay Dent <Kay.Dent@softwire.com>
2024-04-24 16:41:32 +0100

    Implemented rule with 7

--- index.ts
+++ index.ts
@@ -1,19 +1,35 @@
 function divisibleBy(num: number, divisor: number): boolean { return num % divisor == 0 };
 
+const possibleFactors = [3, 5, 7];
+
+const key: {[index: number]: string} = {
+    3: 'Fizz',
+    5: 'Buzz',
+    7: 'Bong'
+}
+
 const fizzBuzz = () => {  
   for (let n = 0; n < 101; n++) {
-    const divisbleByThree: boolean = divisibleBy(n, 3);
-    const divisibleByFive: boolean = divisibleBy(n, 5);
+    let factors: number[] = [];
 
-    let answer = String(n)
+    possibleFactors.forEach((num) => {
+        if (divisibleBy(n, num)) {
+            console.log(`${n} is divisible by ${num}`)
+            factors.push(num)
+        }
+    })
 
-    if (divisbleByThree && divisibleByFive) {
-      answer = 'FizzBuzz'
-    } else if (divisbleByThree) {
-      answer = 'Fizz'
-    } else if (divisibleByFive) {
-      answer = 'Buzz'
-    } 
+    let answer ='';
+
+    if (factors.length > 0) {
+      for (let i = 0; i < factors.length; i++) {
+        if (answer.length < 5) {
+          answer += key[factors[i]]
+        }
+      }
+    } else {
+      answer = String(n)
+    }
 
     console.log(answer);
   }

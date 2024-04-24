2c51b050f66867e5b0697de6b36c9b5a63ed459d
Kay Dent <Kay.Dent@softwire.com>
2024-04-24 17:30:33 +0100

    Implemented 17 rules

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 function divisibleBy(num: number, divisor: number): boolean { return num % divisor == 0 };
 
-const possibleFactors = [3, 5, 7, 11, 13];
+const possibleFactors = [3, 5, 7, 11, 13, 17];
 
 const key: {[index: number]: Function } = {
     3: (answer: string[]) => {
@@ -30,11 +30,13 @@ const key: {[index: number]: Function } = {
         }
         if (!newAnswer.includes('Fezz')) { newAnswer.push('Fezz') } 
         return newAnswer;
-    }
+    },
+    17: (answer: string[]) => answer = answer.reverse()
+
 }
 
 const fizzBuzz = () => {  
-  for (let n = 0; n < 201; n++) {
+  for (let n = 0; n < 260; n++) {
     let factors: number[] = [];
 
     possibleFactors.forEach((num) => {
@@ -49,9 +51,9 @@ const fizzBuzz = () => {
       for (let i = 0; i < factors.length; i++) {
         answer = key[factors[i]](answer)
       }
-    } else {
-      answer.push(String(n))
-    }
+    } 
+
+    if (answer.length == 0) { answer.push(String(n))}
 
     answer.length > 1 ? console.log(answer.join('')) : console.log(answer[0]);
   }

d32283b5f14414fdac870c43388aedc1cd587c32
Kay Dent <Kay.Dent@softwire.com>
2024-04-24 15:16:36 +0100

    Made a fizzBuzz function

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-for (let n = 0; n < 101; n++) {
-  if (n % 3 === 0 && n % 5 === 0) {
-    console.log('FizzBuzz')
-  } else if (n % 3 === 0) {
-    console.log('Fizz')
-  } else if (n % 5 == 0) {
-    console.log('Buzz')
-  } else {
-    console.log(n);
+const fizzBuzz = () => {
+  for (let n = 0; n < 101; n++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (n % 3 === 0) {
+      console.log('Fizz')
+    } else if (n % 5 == 0) {
+      console.log('Buzz')
+    } else {
+      console.log(n);
+    }
   }
 }
+
+fizzBuzz();

e5a75dee1a95baeb15222273d88525a2793a7607
Kay Dent <Kay.Dent@softwire.com>
2024-04-24 15:38:02 +0100

    Added explicit types for divisibleByThree and divisibleByFive

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ function divisibleBy(num: number, divisor: number): boolean { return num % divis
 
 const fizzBuzz = () => {  
   for (let n = 0; n < 101; n++) {
-    const divisbleByThree = divisibleBy(n, 3);
-    const divisibleByFive = divisibleBy(n, 5);
+    const divisbleByThree: boolean = divisibleBy(n, 3);
+    const divisibleByFive: boolean = divisibleBy(n, 5);
     if (divisbleByThree && divisibleByFive) {
       console.log('FizzBuzz')
     } else if (divisbleByThree) {
